fe3a6e1092beb338e276553abd2c2523adfee64d
Schulz Tobias <tsz.springe@gmail.com>
2020-11-11 13:02:52 +0100

    server.js modified

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose';
 import express from 'express';
 
-mongoose.connect('mongodb://localhost:27017/cats', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/cats', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+});
 
 // Express server for routing, logic
 const server = express();
@@ -9,7 +13,7 @@ server.use(express.json());
 
 
 //Cat model (as defined in mongoose)
-const Cat = mongoose.model('Cat', { name: String });
+const Cat = mongoose.model('Cat', { name: String, fur: String, age: Number});
 
 //Express routes
 server.get('/', (request,response) => {
@@ -32,7 +36,16 @@ server.post('/cats/:kittyName', (req,res) =>{
     kitty.save().then(() => res.json(kittyName + ' says meow'));
 });
 
-
+server.patch('/cats/:kittyName', (req,res) => {
+    const { kittyName } = req.params;
+    const updatedCat = req.body;
+    Cat.findOneAndUpdate({ name: kittyName},updatedCat,{ new:true })
+    .then(myNewData => res.json(myNewData))
+    .catch(error => {
+        console.error(error);
+        res.json({error: 'an unexpected error occured'})
+    })
+})
 
 
 
